c99a28eebdcbe684bb3676835933879fd8746eb5
Guilherme Puszkarek <guipuszkarek@gmail.com>
2026-02-01 01:52:44 -0300

    refactor: extract command implementations to dedicated modules and introduce a playground manager.

---
+++ src/commands/run-in-current.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+import { getPlayground } from '../utils/playground-manager';
+
+export const runInCurrent = (context: vscode.ExtensionContext) => {
+    const playground = getPlayground(context);
+    
+    const editor = vscode.window.activeTextEditor;
+    if (editor && (editor.document.languageId === 'typescript' || editor.document.languageId === 'javascript')) {
+            playground.attach(editor.document);
+    } else {
+            vscode.window.showErrorMessage('No active TypeScript/JavaScript file. Use "Summon Fresh Void" to create one.');
+    }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -1,61 +1,18 @@
 import * as vscode from 'vscode';
-import { Playground } from './playground';
-
-let playground: Playground | undefined;
+import { runInCurrent } from './commands/run-in-current';
+import { createNew } from './commands/create-new';
+import { disposePlayground } from './utils/playground-manager';
 
 export const activate = (context: vscode.ExtensionContext) => {
-    console.log('Void extension is now active');
+    console.info('Void extension is now active');
 
-    const runInCurrent = vscode.commands.registerCommand('void.runInCurrent', () => {
-        if (!playground) {
-            playground = new Playground();
-            context.subscriptions.push(playground);
-        }
-        
-        const editor = vscode.window.activeTextEditor;
-        if (editor && (editor.document.languageId === 'typescript' || editor.document.languageId === 'javascript')) {
-             playground.attach(editor.document);
-        } else {
-             vscode.window.showErrorMessage('No active TypeScript/JavaScript file. Use "Summon Fresh Void" to create one.');
-        }
-    });
+    const runInCurrentCommand = vscode.commands.registerCommand('void.runInCurrent', () => runInCurrent(context));
+    const createNewCommand = vscode.commands.registerCommand('void.createNew', () => createNew(context));
 
-    const createNew = vscode.commands.registerCommand('void.createNew', async () => {
-        if (!playground) {
-            playground = new Playground();
-            context.subscriptions.push(playground);
-        }
-
-        const workspaceFolders = vscode.workspace.workspaceFolders;
-        if (workspaceFolders) {
-            const rootPath = workspaceFolders[0].uri.fsPath;
-            const filePath = vscode.Uri.file(require('path').join(rootPath, 'playground.ts'));
-            
-            try {
-                await vscode.workspace.fs.stat(filePath);
-            } catch {
-                const edit = new vscode.WorkspaceEdit();
-                edit.createFile(filePath, { ignoreIfExists: true });
-                edit.insert(filePath, new vscode.Position(0, 0), '// Void Playground\n\nconst x = 10;\nconsole.log(x);\n');
-                await vscode.workspace.applyEdit(edit);
-                const doc = await vscode.workspace.openTextDocument(filePath);
-                await doc.save();
-            }
-            
-            const doc = await vscode.workspace.openTextDocument(filePath);
-            await vscode.window.showTextDocument(doc);
-            playground.attach(doc);
-        } else {
-            vscode.window.showErrorMessage('Open a workspace to summon the Void.');
-        }
-    });
-
-    context.subscriptions.push(runInCurrent);
-    context.subscriptions.push(createNew);
+    context.subscriptions.push(runInCurrentCommand);
+    context.subscriptions.push(createNewCommand);
 }
 
 export const deactivate = () => {
-    if (playground) {
-        playground.dispose();
-    }
+    disposePlayground();
 }
